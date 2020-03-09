f4f4f669938d659da063991041003f0f8aa2bdfb
gitnyasha <marshall.chikari@gmail.com>
2020-03-09 16:55:02 +0200

    made changes to the bot required by the TSE

--- app/main.rb
+++ app/main.rb
@@ -1,7 +1,8 @@
 require "telegram/bot"
+require "dotenv/load"
 require_relative "../lib/bot_logic.rb"
 
-token = "1051702702:AAF2AqS4MA6XP0JbGpCsnUXWdSaPgX0sNJM"
+token = ENV["API"]
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
@@ -9,11 +10,11 @@ Telegram::Bot::Client.run(token) do |bot|
     when "/start"
       bot.api.send_message(chat_id: message.chat.id, text: "Welcome to my book store please select a book you want to read for today!\n/Ruby\n/Java\n/Php\n If you want press /suggest so I can suggest a book for you!")
     when "/Ruby"
-      bot.api.send_document(chat_id: message.chat.id, document: Faraday::UploadIO.new("./lib/test.pdf", "file/pdf"))
+      bot.api.send_document(chat_id: message.chat.id, document: Faraday::UploadIO.new("./lib/ruby.pdf", "file/pdf"))
     when "/Java"
-      bot.api.send_document(chat_id: message.chat.id, document: Faraday::UploadIO.new("./lib/test.pdf", "file/pdf"))
+      bot.api.send_document(chat_id: message.chat.id, document: Faraday::UploadIO.new("./lib/java.pdf", "file/pdf"))
     when "/Php"
-      bot.api.send_document(chat_id: message.chat.id, document: Faraday::UploadIO.new("./lib/test.pdf", "file/pdf"))
+      bot.api.send_document(chat_id: message.chat.id, document: Faraday::UploadIO.new("./lib/phptutorial.pdf", "file/pdf"))
     when "/suggest"
       hint = Languages.suggest
       bot.api.send_message(chat_id: message.chat.id, text: "You can start with this one #{hint}")
