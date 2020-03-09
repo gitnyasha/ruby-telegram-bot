cfa7886c4c1af7438cc0cd37391e27975bc1f330
gitnyasha <marshall.chikari@gmail.com>
2020-03-09 17:01:07 +0200

    fixed issues with the cant merge branches

--- lib/bot_logic.rb
+++
@@ -1,9 +0,0 @@
-class Languages
-  def self.programming
-    @programming = ["/book1 - Ruby", "/book2 - Java", "/book3 - Php"]
-  end
-
-  def self.suggest
-    @suggests = programming.sample
-  end
-end
